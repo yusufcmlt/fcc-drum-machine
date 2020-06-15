4e77911ea681686ce9b991ef0886d1477f3db54a
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-06-16 00:12:26 +0300

    first commit with redux

--- src/App.js
+++ src/App.js
@@ -1,42 +1,56 @@
-import React, { useState } from "react";
-import ButtonsContainer from "./ButtonsContainer";
-
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
 import "./App.css";
 
 function App() {
-  const [displayText, setDisplayText] = useState("Drum is Working");
-
-  const handleDisplay = buttonInfo => {
-    setDisplayText(buttonInfo);
-  };
-
   return (
-    <div id="app-container" className="container">
-      <div id="heading-container" className="row">
-        <h1
-          id="drum-heading"
-          className="col s12 center-align amber-text accent-3"
-          style={{ textShadow: "1px 5px 1px #000" }}
-        >
-          Drum Machine
-        </h1>
-      </div>
-      <div id="drum-machine-row" className="row">
-        <div id="drum-machine" className="col s4 offset-s4  amber z-depth-5">
-          <div id="display-control-container" className="row z-depth-3">
-            <div
-              id="display"
-              className="col s12 black center-align"
-              style={{
-                marginTop: "5px"
-              }}
-            >
-              <h6 style={{ padding: "20px" }}>{displayText}</h6>
-            </div>
-          </div>
-          <ButtonsContainer displayHandler={handleDisplay} />
-        </div>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
     </div>
   );
 }
