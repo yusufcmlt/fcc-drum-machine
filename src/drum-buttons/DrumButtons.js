6f97a83a30e95fbc8436125bcad412c6e20ab9bb
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-06-28 13:45:46 +0300

    create component

---
+++ src/drum-buttons/DrumButtons.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import DrumDisplay from "../drum-display/DrumDisplay";
+
+import "./DrumButtons.css";
+
+export default function DrumButtons() {
+  return (
+    <div className="drum-container">
+      <DrumDisplay />
+    </div>
+  );
+}
---
+++ src/drum-display/DrumDisplay.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./DrumDisplay.css";
+
+export default function DrumDisplay() {
+  return (
+    <div>
+      <div>TEST</div>
+      <div>TEST</div>
+    </div>
+  );
+}
